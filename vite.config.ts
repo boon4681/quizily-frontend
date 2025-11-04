bf23ed6933b52ea9f34de632627faf4c3b8771d8
boon4681 <boon4681dev@gmail.com>
2025-11-04 20:55:49 +0700

    feat: deployment reverse proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	server: {
-		proxy: {
-			'/api': {
-				target: "http://localhost:3000",
-				changeOrigin: true
-			}
-		}
-	}
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    const isProd = mode === 'production'
+    console.log(`Running on ${isProd ? "Production" : "Localhost"}`)
+    return {
+        plugins: [tailwindcss(), sveltekit()],
+        server: {
+            proxy: {
+                '/api': {
+                    target: isProd ? env.VITE_BACKEND ?? 'http://localhost:3000' : 'http://localhost:3000',
+                    rewrite: (p) => p.replace('/api/', '/')
+                }
+            }
+        },
+    }
 });
